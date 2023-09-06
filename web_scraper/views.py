617768c1f4186e28c1c3c09cc879b287c78f5245
Nathan VanSickle <ndvans03@gmail.com>
2023-09-06 14:08:53 -0400

    add basic GuitarCenter scraper

--- web_scraper/views.py
+++ web_scraper/views.py
@@ -7,7 +7,7 @@ def run_scrapers(request):
     try:
         url = 'https://www.guitarcenter.com/All-Deals.gc?N=18144'
         result = subprocess.run(['python3', 'web_scraper/guitarcenter_scraper.py', url], check=True, text=True, capture_output=True)
-        return JsonResponse({'result': result.stdout})
+        return JsonResponse({'results': result.stdout})
     except subprocess.CalledProcessError:
         return JsonResponse({'result': 'Error executing web scrapers'})
 
